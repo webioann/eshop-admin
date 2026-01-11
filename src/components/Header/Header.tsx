24e836224afc0365345256623989feb1b91119ae
webioann <webioann@gmail.com>
2026-01-11 14:18:40 +0200

    header grid skeleton was created

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
-import styles from './header.module.scss'
+import styles from './header.module.scss';
+import { CgList } from "react-icons/cg";
+import { FaRegUserCircle } from "react-icons/fa";
 
 function Header() {
     return (
         <div className={styles.header}>
-            Header
+            <div className={styles.left_side}>
+                <CgList size={30} color="#333" />
+                <h3 className={styles.header_title}>Admin</h3>
+            </div>
+            <span className={styles.user_icon}>
+                <FaRegUserCircle size={30} color="#333" />
+            </span>
+                
         </div>
     )
 }
